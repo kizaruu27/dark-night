14a4bde111ef388fe2be3d29c289bd11d603af54
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-02-08 15:29:25 +0700

    player controller

---
+++ Dark Night/Assets/Script/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float camerSensivity = 3.5f;
+    [SerializeField] bool lockCursor = true;
+    [SerializeField] [Range(0.0f, 0.5f)] float cameraSmoothTime = 0.03f;
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
+
+    float cameraPitch = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseLook();
+    }
+
+    void mouseLook() {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, cameraSmoothTime);
+        cameraPitch -= currentMouseDelta.y * camerSensivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * camerSensivity);
+    }
+}
---
+++ Dark Night/Assets/Script/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float baseSpeed = 6f;
+    [SerializeField] float speed;
+    [SerializeField] float runSpeed = 12f;
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    [SerializeField] float gravity = -13f;
+    CharacterController controller = null;
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
+    float velocityY = 0.0f;
+    bool isWalking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        speed = baseSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() {
+        Vector2 targetDir = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if (controller.isGrounded) {
+            velocityY = 0.0f;
+        }
+
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * speed + Vector3.up * velocityY; 
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speed = runSpeed;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            speed = baseSpeed;
+        }
+    }
+}
