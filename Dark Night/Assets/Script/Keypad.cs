8b8390ffc5d87df4018d19b6b17efe94d944628e
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-02-11 22:57:29 +0700

    keypad mechanics

---
+++ Dark Night/Assets/Script/Keypad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] Text Answer;
+    string ans = "123";
+    public int digits = 0;
+    PuzzleManager puzzleManager;
+
+    public void Number(int number) {
+        Answer.text += number.ToString();
+    }
+
+    public void Execute() {
+        if (Answer.text == ans) {
+            Debug.Log("Password Benar");
+        } else {
+            Debug.Log("Password Salah");
+        }
+    }
+
+    public void ResetPassword() {
+        Answer.text = null;
+    }
+}
--- Dark Night/Assets/Script/PuzzleManager.cs
+++ Dark Night/Assets/Script/PuzzleManager.cs
@@ -4,15 +4,23 @@ public class PuzzleManager : MonoBehaviour
 {
      public Objects puzzle, door;
      [SerializeField] GameObject doorOpen, doorClosed;
+     public GameObject Keypad;
      UIManager uIManager;
      AnimationManager anim;
+     public bool isUsingKeypad;
 
     private void Start() {
+        Time.timeScale = 1;
+
         uIManager = FindObjectOfType<UIManager>();
         anim = FindObjectOfType<AnimationManager>();
 
         puzzle.puzzleKey = 0;
         puzzle.puzzleKeyPicked = false;
+    }
+
+    private void Update() {
+        DisableKeypad();
     }
 
     public void PickUpPuzzleKey() {
@@ -26,6 +34,24 @@ public class PuzzleManager : MonoBehaviour
             doorClosed.SetActive(false);
         } else if (puzzle.puzzleKeyPicked == false) {
             uIManager.DoorMessege();
+        }
+    }
+
+    public void DisplayKeypad() {
+        isUsingKeypad = true;
+        Keypad.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void DisableKeypad() {
+        if (Input.GetKey(KeyCode.Escape) && isUsingKeypad) {
+            isUsingKeypad = false;
+            Keypad.SetActive(false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;   
         }
     }
 }
